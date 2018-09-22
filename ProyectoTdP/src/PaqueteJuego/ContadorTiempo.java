31ecc54487044404fad010779e8ccefcc6cbdb31
Ignacio Martín Chiaravalle <nachochiara@gmail.com>
2018-09-22 10:14:10 -0300

    Aumento el thread.sleep() para que los enemigos vayan un poco más lento.

--- ProyectoTdP/src/PaqueteJuego/ContadorTiempo.java
+++ ProyectoTdP/src/PaqueteJuego/ContadorTiempo.java
@@ -11,7 +11,7 @@ public class ContadorTiempo extends Thread {
 	public void run() {
 		while(true){
 			try {
-				Thread.sleep(20);
+				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
