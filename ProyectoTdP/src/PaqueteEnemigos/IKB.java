a4cadf9ab73e7f6004187bc6a8247e6c4001b928
DESKTOP-1NR5180\gero <geroleleant@gmail.com>
2018-10-07 18:39:56 -0300

    c2pl

--- ProyectoTdP/src/PaqueteEnemigos/IKB.java
+++ ProyectoTdP/src/PaqueteEnemigos/IKB.java
@@ -21,6 +21,8 @@ public class IKB extends Inteligencia {
 				pos.setLocation(pos.x - velocidad, pos.y);
 		}
 		pos.setLocation(pos.x, pos.y + velocidad);
+		if (pos.y > Ymax * 0.9)
+			pos.y = 0;
 	}
 	
 	public boolean iniciaConMovimiento() {
